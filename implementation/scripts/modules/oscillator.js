9f9a8e9c3c71679b8b7937affb25253148bb20de
Oskar Eriksson <oskar.det@gmail.com>
2013-04-29 17:37:31 +0200

    added statics

--- implementation/scripts/modules/oscillator.js
+++ implementation/scripts/modules/oscillator.js
@@ -1,43 +1,51 @@
-define(["context"], function(context) {
+define(["context", "statics"], function(context, STATICS) {
 
     return function() {
-        var waveform,
-            pitch = 440,
-            glide = 0.09,
+        var pitch = 440,
+            glide = 0.05,
             oscillator,
             destination,
             playing = false,
-            id = Math.random(),
             currentNote = -1,
-            controllers = {};
+            controllers = {},
+            waveform = 0;
 
         function start(note, time) {
+
+            //stop previous note if it's still playing
             if (playing && glide === 0) {
                 stop();
-            } else if (playing && glide > 0) {
-                pitch = getFrequency(note);
-                oscillator.frequency.setTargetAtTime(pitch, context.currentTime, glide);
-                oscillator.type = "sawtooth";
-                oscillator.start(time);
-                playing = true;
-                currentNote = note;
-                return;
             }
+
+            //create a new oscillator if there's none playing (oscillators are one shot in Web Audio)
+            if (!playing) {
+                oscillator = context.createOscillator();
+                oscillator.connect(destination);
+                oscillator.type = STATICS.waveforms[waveform];
+                oscillator.detune.value = Math.random() * 0.1;
+            }
+
+            //calculate the frequency of the note we're going to play
             pitch = getFrequency(note);
-            oscillator = context.createOscillator();
-            oscillator.connect(destination);
-            oscillator.frequency.value = pitch;
-            oscillator.type = "sawtooth";
+
+            //if glide is on and a note is currently playing, we need to tween the frequency of the currently playing note, or else just set the frequency
+            if (playing && glide > 0) {
+                oscillator.frequency.setTargetAtTime(pitch, context.currentTime, glide);
+            } else {
+                oscillator.frequency.value = pitch;
+            }
             oscillator.start(time);
             playing = true;
             currentNote = note;
         }
 
         function getFrequency(note) {
+            //convert midi notes 0-127 to the corresponding frequency
             return 8.1757989156 * Math.pow(2.0, note / 12.0);
         }
 
         function stop(note, time) {
+            //don't stop the oscillator if it's another key that has been lifted
             if (note !== undefined && note !== currentNote) {
                 return;
             }
@@ -65,7 +73,6 @@ define(["context"], function(context) {
             start: start,
             stop: stop,
             setValue: setValue,
-            id: id,
             connect: connect,
             disconnect: disconnect
         };
